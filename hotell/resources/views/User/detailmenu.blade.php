6607c99fe56e107561d29d2301ff5ed7cc759e8a
tiyanlestari <redmitiyan@gmail.com>
2024-02-13 15:16:21 +0700

    lotushotel

--- hotell/resources/views/User/detailmenu.blade.php
+++ hotell/resources/views/User/detailmenu.blade.php
@@ -9,16 +9,20 @@
             <h2 style="font-size: 20px; font-weight: bold; margin-left : 100px;">LUXURY ROOM</h2>
             <div style="position: absolute; top: 100px; left: 20px;">
                 <a href="{{ route('menu') }}">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" style="transform: rotate(180deg);">
-                        <path fill="currentColor" d="M10 17l5-5-5-5v10z"/>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24"
+                        style="transform: rotate(180deg);">
+                        <path fill="currentColor" d="M10 17l5-5-5-5v10z" />
                     </svg>
                 </a>
             </div>
             <div style="display: flex; align-items: center; margin-top: 10px;">
                 <img src="images/kamar1.png" alt="Logo" width="500" style="margin-right: 20px; margin-left: 100px;">
                 <div style="margin-left: 30px; margin-bottom: 50px;">
-                    <p style="font-family: 'Poppins', sans-serif; width: 700px;">
-                        Kamar Hotel Luxur Room adalah jenis kamar hotel yang menawarkan pengalaman menginap mewah dan nyaman bagi para tamu. Kamar ini biasanya merupakan kamar yang lebih besar dari kamar standar dan menawarkan fasilitas dan layanan tambahan yang dirancang untuk meningkatkan kenyamanan dan kepuasan tamu.
+                    <p style="font-family: 'Poppins', sans-serif; width: 580px;">
+                        Kamar Hotel Luxur Room adalah jenis kamar hotel yang menawarkan pengalaman menginap mewah dan nyaman
+                        bagi para tamu. Kamar ini biasanya merupakan kamar yang lebih besar dari kamar standar dan
+                        menawarkan fasilitas dan layanan tambahan yang dirancang untuk meningkatkan kenyamanan dan kepuasan
+                        tamu.
                     </p>
                     <div style="margin-left: -2px;">
                         <div style="margin-left: 10px;">
@@ -40,24 +44,25 @@
             </div>
             <div style="display: flex; align-items: center; margin-top: -100px;">
                 <div style="margin-left: auto; margin-right: 100px; margin-top: 10px;">
-                    <p style="font-size: 25px; font-weight: bold;">Rp. 4.500.000 <span style="font-style: italic; color: #D88F00;">/ malam</span></p>
+                    <p style="font-size: 25px; font-weight: bold;">Rp. 4.500.000 <span
+                            style="font-style: italic; color: #D88F00;">/ malam</span></p>
                 </div>
             </div>
             <div style="margin-top: 10px;">
                 <button type="button" class="btn btn-primary btn-lg"
-                    style="width: 200px; margin-left: 1150px;">DETAIL</button>
+                    style="width: 200px; margin-left: 1000px;">DETAIL</button>
             </div>
         </div>
     </div>
     <br>
     <br>
-    <div style="border-bottom: 1px solid black; width: 87%; margin-left: 100px; margin-top: 10px;"></div>
+    <div style="border-bottom: 1px solid black; width: 87%; margin-left: 90px; margin-top: 10px;"></div>
     <br>
     <h2 style="font-size: 20px; font-weight: bold; margin-left : 100px;">COUPLE ROOM</h2>
-    <div style="display: flex; align-items: center; margin-top: 10px;"> <!-- Adjusted margin-top here -->
+    <div style="display: flex; align-items: center; margin-top: 10px;">
         <img src="images/kamar2.png" alt="Logo" width="500" style="margin-right: 20px; margin-left: 100px;">
         <div style="margin-left: 30px; margin-bottom: 90px;">
-            <p style="font-family: 'Poppins', sans-serif; width: 700px;">
+            <p style="font-family: 'Poppins', sans-serif; width: 580px;">
                 Kamar Couple Room yang indah ini menawarkan nuansa romantis
                 dengan dekorasi yang lembut dan sentuhan modern. Dengan pemandangan kota yang menakjubkan dari jendela,
                 suasana ini menciptakan pengalaman menginap yang tak terlupakan bagi pasangan yang mencari ketenangan.</p>
@@ -81,25 +86,25 @@
     </div>
     <div style="display: flex; align-items: center; margin-top: -100px;">
         <div style="margin-left: auto; margin-right: 100px; margin-top: 10px;"> <!-- Adjusted margin-top here -->
-            <p style="font-size: 25px; font-weight: bold;">Rp. 3.00.000 <span style="font-style: italic; color: #D88F00;">/ malam</span></p>
+            <p style="font-size: 25px; font-weight: bold;">Rp. 3.00.000 <span style="font-style: italic; color: #D88F00;">/
+                    malam</span></p>
         </div>
     </div>
     <div style="margin-top: 10px;">
-        <button type="button" class="btn btn-primary btn-lg"
-            style="width: 200px; margin-left: 1150px;">DETAIL</button>
+        <button type="button" class="btn btn-primary btn-lg" style="width: 200px; margin-left: 1000px;">DETAIL</button>
     </div>
     </div>
     </div>
     </div>
     <br>
     <br>
-    <div style="border-bottom: 1px solid black; width: 87%; margin-left: 100px; margin-top: 10px;"></div>
+    <div style="border-bottom: 1px solid black; width: 87%; margin-left: 90px; margin-top: 10px;"></div>
     <br>
     <h2 style="font-size: 20px; font-weight: bold; margin-left : 100px;">STANDARD ROOM</h2>
     <div style="display: flex; align-items: center; margin-top: 10px;">
         <img src="images/kamar3.png" alt="Logo" width="500" style="margin-right: 20px; margin-left: 100px;">
         <div style="margin-left: 50px; margin-bottom: 90px;">
-            <p style="font-family: 'Poppins', sans-serif; width: 700px;">
+            <p style="font-family: 'Poppins', sans-serif; width: 580px;">
                 Kamar Standard Room yang nyaman ini memberikan pengalaman
                 menginap yang menyenangkan dengan desain fungsional dan suasana yang hangat. Dilengkapi dengan fasilitas
                 dasar yang memadai, kamar ini adalah pilihan ideal bagi tamu yang mencari kenyamanan dan keseimbangan antara
@@ -123,27 +128,28 @@
         </div>
     </div>
     <div style="display: flex; align-items: center; margin-top: -100px;">
-        <div style="margin-left: auto; margin-right: 100px; margin-top: 10px;"> <!-- Adjusted margin-top here -->
-            <p style="font-size: 25px; font-weight: bold;">Rp. 2.200.000 <span style="font-style: italic; color: #D88F00;">/ malam</span></p>
+        <div style="margin-left: auto; margin-right: 100px; margin-top: 10px;">
+            <p style="font-size: 25px; font-weight: bold;">Rp. 2.200.000 <span style="font-style: italic; color: #D88F00;">/
+                    malam</span></p>
         </div>
     </div>
     <div style="margin-top: 10px;">
-        <button type="button" class="btn btn-primary btn-lg"
-            style="width: 200px; margin-left: 1150px;">DETAIL</button>
+        <button type="button" class="btn btn-primary btn-lg" style="width: 200px; margin-left: 1000px;">DETAIL</button>
     </div>
     </div>
     </div>
     </div>
     <br>
     <br>
-    <div style="border-bottom: 1px solid black; width: 87%; margin-left: 100px; margin-top: 10px;"></div>
+    <div style="border-bottom: 1px solid black; width: 87%; margin-left: 90px; margin-top: 10px;"></div>
     <br>
     <h2 style="font-size: 20px; font-weight: bold; margin-left: 100px;">FAMILY ROOM</h2>
     <div style="display: flex; align-items: center; margin-top: 10px;">
         <img src="images/kamar4.png" alt="Logo" width="500" style="margin-right: 20px; margin-left: 100px;">
         <div style="margin-left: 30px; margin-bottom: 90px;">
-            <p style="font-family: 'Poppins', sans-serif; width: 700px;">
-                Kamar Hotel Family Room adalah jenis kamar yang dirancang khusus untuk keluarga atau tamu dengan jumlah anggota yang lebih besar. Kamar ini menyediakan ruang yang luas dan fasilitas tambahan.
+            <p style="font-family: 'Poppins', sans-serif; width: 580px;">
+                Kamar Hotel Family Room adalah jenis kamar yang dirancang khusus untuk keluarga atau tamu dengan jumlah
+                anggota yang lebih besar. Kamar ini menyediakan ruang yang luas dan fasilitas tambahan.
             </p>
             <div style="margin-left: -2px;">
                 <div style="margin-left: 10px;">
@@ -164,13 +170,13 @@
         </div>
     </div>
     <div style="display: flex; align-items: center; margin-top: -100px;">
-        <div style="margin-left: auto; margin-right: 100px; margin-top: 10px;"> <!-- Adjusted margin-top here -->
-            <p style="font-size: 25px; font-weight: bold;">Rp. 2.200.000 <span style="font-style: italic; color: #D88F00;">/ malam</span></p>
+        <div style="margin-left: auto; margin-right: 100px; margin-top: 10px;">
+            <p style="font-size: 25px; font-weight: bold;">Rp. 2.200.000 <span
+                    style="font-style: italic; color: #D88F00;">/ malam</span></p>
         </div>
     </div>
     <div style="margin-top: 10px;">
-        <button type="button" class="btn btn-primary btn-lg"
-            style="width: 200px; margin-left: 1150px;">DETAIL</button>
+        <button type="button" class="btn btn-primary btn-lg" style="width: 200px; margin-left: 1000px;">DETAIL</button>
     </div>
     </div>
     </div>
