8a4d8f62c2fbfa7f9129b79f7526b2feb845fd23
tiyanlestari <redmitiyan@gmail.com>
2024-02-13 14:00:31 +0700

    detailmenu

--- hotell/resources/views/auth/register.blade.php
+++ hotell/resources/views/auth/register.blade.php
@@ -1,13 +1,17 @@
 @extends('layouts.side')
+
 @section('content')
     <section class="section-account parallax"
         style="background-image: url('images/register.png'); background-size: cover; background-position: center center;">
         <div class="awe-overlay"></div>
         <div class="container">
             <div class="login-register">
-                <div    
+                <div
                     style="width: 600px; height: 660px; background: rgba(217, 217, 217, 0.21); border-radius: 40px; backdrop-filter: blur(30px); margin-left: auto; margin-right: auto;">
                     <div class="text text-center" style="margin-top: -100px">
+                        <a href="{{ route('auth.login') }}" style="position: absolute; top: 20px; left: 20px; font-size: 18px; color: rgb(255, 255, 255);">
+                            &#8592; Kembali
+                        </a>
                         <img src="images/logo-1.png" width="200" height="200" style="margin-top: 50px">
                         <h2 style="font-family: 'Poppins', sans-serif; margin-top: 20px;">REGISTER FORM</h2>
                         <p style="font-family: 'Poppins', sans-serif; font-weight: 300; margin-top: 10px;">Jika Anda tidak memiliki akun di Everywhere, daftar sekarang!</p>
@@ -46,7 +50,6 @@
                             </form>
                         </div>
                     </div>
-                    
                 </div>
             </div>
         </div>
