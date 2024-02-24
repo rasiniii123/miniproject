faa8f3b2589043b2a8ec28ee7d619c0b9328fc85
rasiniii123 <152830056+rasiniii123@users.noreply.github.com>
2024-02-24 19:16:51 +0700

    tampilan histori

--- hotell/routes/web.php
+++ hotell/routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\RoomController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DetailController;
 use App\Http\Controllers\UlasanController;
-use App\Http\Controllers\HistoriController;
+
 use App\Http\Controllers\PesananController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TentangController;
@@ -14,6 +14,7 @@ use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\DetailmenuController;
+use App\Http\Controllers\HistoriController;
 use App\Http\Controllers\AdminDashboardController;
 
 
@@ -22,7 +23,7 @@ use App\Http\Controllers\AdminDashboardController;
 
 
 
-
+Route::get('/histori', [HistoriController::class, 'index'])->name('histori');
 Route::get('/login', [UserController::class, 'index'])->name('login');
 Route::post('/login', [UserController::class, 'store'])->name('login.submit');
 Route::get('/register', [RegisterController::class, 'index'])->name('auth.register');
@@ -39,6 +40,7 @@ Route::middleware('auth')->group(function () {
     Route::get('/tentangkami', [TentangController::class, 'index'])->name('tentang.index');
     Route::get('/detail/{id}', [DetailController::class, 'index'])->name('detail.index');
     Route::get('/pesanan', [PesananController::class, 'index'])->name('pesanan');
+
 });
 
 Route::middleware(['auth', 'checkrole:admin'])->group(function () {
