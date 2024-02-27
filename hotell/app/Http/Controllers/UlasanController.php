9efa1027835809efe59a1fd09bc46d4092db06dc
susilowati11 <143865726+susilowati11@users.noreply.github.com>
2024-02-27 14:48:55 +0700

    no try catch

--- hotell/app/Http/Controllers/UlasanController.php
+++ hotell/app/Http/Controllers/UlasanController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Room;
 use App\Models\User;
 use App\Models\Ulasan;
 use Illuminate\Http\Request;
@@ -25,8 +24,7 @@ class UlasanController extends Controller
     public function create()
     {
 
-            return view('ulasan');
-
+        return view('ulasan');
     }
 
     /**
@@ -35,35 +33,27 @@ class UlasanController extends Controller
     public function store(Request $request)
     {
         // Validate the request data
-        // dd($request);
         $validatedData = $request->validate([
             'rating' => 'required|numeric',
             'ulasan' => 'required|string',
             'pesanan_id' => 'required|exists:pesanan,id',
             'user_id' => 'required|exists:users,id',
         ]);
-    
-        try {
-            $semuaKamar = Room::all();
 
-            // Simpan ulasan ke dalam database
-            foreach ($semuaKamar as $kamar) {
-            $ulasan = new Ulasan;
-            $ulasan->rating = $validatedData['rating'];
-            $ulasan->ulasan = $validatedData['ulasan'];
-            $ulasan->pesanan_id = $validatedData['pesanan_id'];
-            $ulasan->user_id = $validatedData['user_id'];
-            $ulasan->save();
-            }
-            // Jika penyimpanan berhasil, arahkan ke halaman detail
-            return Redirect::route('detail.index', ['id' => $validatedData['pesanan_id']])
-                ->with('success', 'Ulasan berhasil disimpan');
-        } catch (\Exception $e) {
-            // Jika terjadi kesalahan, tangani dengan memberikan pesan kesalahan
-            return response()->json(['error' => $e->getMessage()], 500);
-        }
+        // Simpan ulasan ke dalam database
+        $ulasan = new Ulasan;
+        $ulasan->rating = $validatedData['rating'];
+        $ulasan->ulasan = $validatedData['ulasan'];
+        $ulasan->pesanan_id = $validatedData['pesanan_id'];
+        $ulasan->user_id = $validatedData['user_id'];
+        $ulasan->save();
+
+        // Jika penyimpanan berhasil, arahkan ke halaman detail
+        return Redirect::route('detail.index', ['id' => $validatedData['pesanan_id']])
+            ->with('success', 'Ulasan berhasil disimpan');
     }
-    
+
+
 
 
 
